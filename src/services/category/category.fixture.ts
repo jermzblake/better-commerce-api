28d050185adde1ea15ce1d018843f515cbdf5e36
jermzblake <jermzblake@gmail.com>
2023-04-19 08:56:46 -0400

    remove sizes from Categories fixture

--- src/services/category/category.fixture.ts
+++ src/services/category/category.fixture.ts
@@ -1,10 +1,6 @@
 import { CategoryEntity } from '../../types'
 
 export const Categories: CategoryEntity[] = [
-  {name: 'small'},
-  {name: 'medium'},
-  {name: 'large'},
-  {name: 'x-large'},
   {name: 'men'},
   {name: 'women'},
   {name: 'unisex'},
