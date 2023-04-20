3608a7a7185e70c7c10052c74d1ec11b569cc0af
jermzblake <jermzblake@gmail.com>
2023-04-19 20:51:38 -0400

    refactor category service

--- src/services/category/category.service.ts
+++ src/services/category/category.service.ts
@@ -1,8 +1,8 @@
-import { CategoryEntity } from '../../types'
+import { Category } from '@prisma/client'
 import * as categoryRepository from  '../../repositories/category/category.repository'
 import { Categories } from './category.fixture'
 
-export const createCategory = async (categoryName: string): Promise<CategoryEntity> => {
+export const createCategory = async (categoryName: string): Promise<Category> => {
   const category = await categoryRepository.createCategory(categoryName)
   return category
 }
