a5dedac9edfd72244bb99e38af0d6dd2de57dae1
jermzblake <jermzblake@gmail.com>
2023-04-22 08:50:53 -0400

    refactor fetchProducts method

--- src/repositories/product/product.repository.ts
+++ src/repositories/product/product.repository.ts
@@ -1,24 +1,28 @@
-import { Product } from '@prisma/client'
 import { db } from '../../server/db'
 import { ProductEntity, CategoryEntity, PagingParams } from '../../types'
 
 export const fetchProducts = async (params?: PagingParams) => {
   if (params) {
     const offset = (params.page < 1 ? 1 : params.page - 1) * params.pageSize
-    const limit = params.pageSize
-    const result = await db.product.findMany({ where: {deleted_at: null}, skip: offset, take: limit, orderBy: { created_at: 'desc'} })
-    return result
+    const result = await db.product.findMany({
+      where: { deleted_at: null },
+      skip: offset,
+      take: params.pageSize,
+      orderBy: { created_at: 'desc' },
+    })
+    params.data = result
+    return params
   }
-  const result = await db.product.findMany({ where: {deleted_at: null} })
+  const result = await db.product.findMany({ where: { deleted_at: null } })
   return result
 }
 
 export const fetchProductById = async (productId: string) => {
-  const result = await db.product.findFirst({ where: {id: productId}, include: { categories: true}})
+  const result = await db.product.findFirst({ where: { id: productId }, include: { categories: true } })
   return result
 }
 
-export const createProduct = async (product: ProductEntity, categories?: CategoryEntity[]): Promise<Product> => {
-  const result = await db.product.create({ data: {...product, categories: {connect: categories}} })
+export const createProduct = async (product: ProductEntity, categories?: CategoryEntity[]) => {
+  const result = await db.product.create({ data: { ...product, categories: { connect: categories } } })
   return result
 }
