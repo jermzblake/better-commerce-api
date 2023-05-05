266eba24915801df2d1c2040e3b04a08b7ac5fc6
jermzblake <jermzblake@gmail.com>
2023-05-04 21:18:44 -0400

    remove password from customer types

--- src/types/customer/customer.types.ts
+++ src/types/customer/customer.types.ts
@@ -4,7 +4,6 @@ export interface Customer extends BusinessObject {
   email: string
   firstName: string
   lastName: string
-  passwordHash?: string
   phoneNumber?: string
   orders?: Order[]
 }
@@ -13,7 +12,6 @@ export interface CustomerEntity extends Entity {
   email: string
   first_name: string
   last_name: string
-  password_hash: string
   phoneNumber?: string
   orders?: OrderEntity[]
 }
