a613172e31c3f02edc6f3d43c03b1b146456495d
jermzblake <jermzblake@gmail.com>
2023-05-22 13:13:51 -0400

    clean code

--- src/services/order/order.service.ts
+++ src/services/order/order.service.ts
@@ -1,14 +1,14 @@
 import { Order, OrderItem, PagingParams } from '../../types'
 import { Order as OrderModel } from '@prisma/client'
 import * as orderRepository from '../../repositories/order/order.repository'
-import  { BadRequestError, NotFoundError } from '../../lib'
+import { BadRequestError, NotFoundError } from '../../lib'
 import { mapOrderEntityFromOrder, mapOrderFromOrderModel } from '../../dataMappers/order/order.mappers'
 import { createOrderItem, deleteOrderItem } from './order.item.service'
 import { findProductById } from '../product/product.service'
 
-export const createOrder = async (order: Order): Promise<Order>  => {
+export const createOrder = async (order: Order): Promise<Order> => {
   const pendingOrderItems = order.orderItems
-    // order.status = 'pending'
+  // order.status = 'pending'
   const orderEntity = mapOrderEntityFromOrder(order)
   const order_db_response = await orderRepository.createOrder(orderEntity)
   let createdOrderItems: OrderItem[] = []
@@ -16,7 +16,7 @@ export const createOrder = async (order: Order): Promise<Order>  => {
     const product = await findProductById(item.productId)
     if (!product.quantity || product.quantity <= 0) {
       throw new NotFoundError(__filename, `Product no longer available`)
-    } else if (product.quantity as number < item.quantity){
+    } else if ((product.quantity as number) < item.quantity) {
       //TODO handle this scenario other than throwing an exception
       throw new BadRequestError(__filename, `There are not enough products with ID ${product.id} to fulfil your order`)
     }
@@ -25,13 +25,16 @@ export const createOrder = async (order: Order): Promise<Order>  => {
     const orderItem = await createOrderItem(item)
     createdOrderItems.push(orderItem)
   }
-  return {...mapOrderFromOrderModel(order_db_response), orderItems: createdOrderItems}
+  return { ...mapOrderFromOrderModel(order_db_response), orderItems: createdOrderItems }
 }
 
 export const findOrders = async (params?: PagingParams): Promise<PagingParams | Order[]> => {
   const result = await orderRepository.fetchOrders(params)
   if (params) {
-    return {...(result as PagingParams), data: (result as PagingParams)?.data?.map((order: OrderModel) => mapOrderFromOrderModel(order))}
+    return {
+      ...(result as PagingParams),
+      data: (result as PagingParams)?.data?.map((order: OrderModel) => mapOrderFromOrderModel(order)),
+    }
   }
   return (result as OrderModel[]).map((order) => mapOrderFromOrderModel(order))
 }
@@ -44,13 +47,13 @@ export const findOrderById = async (orderId: string): Promise<Order> => {
   return mapOrderFromOrderModel(db_response)
 }
 
-export const updateOrder = async (orderId: string, order: Order): Promise<Order>  => {
+export const updateOrder = async (orderId: string, order: Order): Promise<Order> => {
   const orderEntity = mapOrderEntityFromOrder(order)
   const db_response = await orderRepository.updateOrder(orderId, orderEntity)
   return mapOrderFromOrderModel(db_response)
 }
 
-export const deleteOrder = async (orderId: string): Promise<Order>  => {
+export const deleteOrder = async (orderId: string): Promise<Order> => {
   const [db_response] = await orderRepository.deleteOrder(orderId)
   return mapOrderFromOrderModel(db_response)
 }
