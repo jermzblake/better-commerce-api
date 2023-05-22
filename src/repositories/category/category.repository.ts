55cbe6943afd4c0f0198c2e711a46a2dff91d348
jermzblake <jermzblake@gmail.com>
2023-05-22 14:10:47 -0400

    refactor

--- src/repositories/category/category.repository.ts
+++ src/repositories/category/category.repository.ts
@@ -2,11 +2,6 @@ import { db } from '../../server/db'
 import { Category } from '@prisma/client'
 import { CategoryEntity } from '../../types'
 
-export const findCategoryByName = async (categoryName: string): Promise<Category | null> => {
-  const result = await db.category.findFirst({ where: { name: categoryName }})
-  return result
-}
-
 export const createCategory = async (categoryName: string): Promise<Category> => {
   const category = await db.category.create({ data: {name: categoryName} })
   return category
