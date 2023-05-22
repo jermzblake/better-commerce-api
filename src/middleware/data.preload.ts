60bbd3dd7a43592220ae77c76b048f2bb1760dbc
jermzblake <jermzblake@gmail.com>
2023-05-22 13:58:07 -0400

    add preloadBulkStatuses to data.preload

--- src/middleware/data.preload.ts
+++ src/middleware/data.preload.ts
@@ -1,6 +1,9 @@
 import { preloadBulkCategories } from '../services/category/category.service'
 import { preloadBulkSizes } from '../services/size/size.service'
+import { preloadBulkStatuses } from '../services/status/status.service'
+
 export const dataPreload = async () => {
   await preloadBulkCategories()
   await preloadBulkSizes()
+  await preloadBulkStatuses()
 }
