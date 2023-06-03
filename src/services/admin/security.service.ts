ee1191818cea01dc2b94018c0aa1d1e202f6a5ba
jermzblake <jermzblake@gmail.com>
2023-06-03 11:38:21 -0400

    add passwordHash function to security service

--- src/services/admin/security.service.ts
+++ src/services/admin/security.service.ts
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 const passphrase = process.env.API_KEY
+const SALT_ROUNDS = 8
 
 export const getJwtTokenFromValue = (value: any) => {
     return jwt.sign(value, passphrase as string)
@@ -10,6 +12,11 @@ export const getValueFromJwt = (token: string) => {
     const result = jwt.verify(token, passphrase as string)
     return result
 
+}
+
+/* password hashing */
+export const hashPassword = (password: string) => {
+    return bcrypt.hashSync(password, SALT_ROUNDS)
 }
 
 /* Encode and Decode can be used to implement encrypt/decrypt methods */
