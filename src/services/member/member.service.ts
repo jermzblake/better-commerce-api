c24eb1f94a9617b6203d621ca3e61aa641909074
jermzblake <jermzblake@gmail.com>
2024-01-09 08:02:24 -0500

    delete member password in createMember

--- src/services/member/member.service.ts
+++ src/services/member/member.service.ts
@@ -3,11 +3,12 @@ import { Member as MemberModel } from '@prisma/client'
 import * as memberRepository from '../../repositories/member/member.repository'
 import { NotFoundError } from '../../lib'
 import { mapMemberFromMemberModel, mapMemberEntityFromMember } from '../../dataMappers/member/member.mappers'
-import { hashPassword } from '../../services/admin/security.service'
+import { hashPassword, comparePassword } from '../../services/admin/security.service'
 
 export const createMember = async (member: Member) => {
   if (member.password) {
     member.passwordHash = hashPassword(member.password)
+    delete member.password
   }
   const MemberEntity = mapMemberEntityFromMember(member)
   const db_response: MemberModel = await memberRepository.createMember(MemberEntity)
