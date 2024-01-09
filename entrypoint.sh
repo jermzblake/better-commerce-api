0dd6ef1352bfd140acce583ba46c52ff55e91622
jermzblake <jermzblake@gmail.com>
2024-01-09 08:27:32 -0500

    update entrypoint scripts for production

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Feel free to add migrations or whatever here
-yarn migrate:dev init
+# yarn migrate:dev init
 # In production don't run migrate:dev BUT do run migrate:prod
-#yarn migrate:prod
+yarn migrate:prod
 yarn start
