0dba467a67a675e6d279ec6a465f38eaf0c8743b
jermzblake <jermzblake@gmail.com>
2024-01-18 21:47:40 -0500

    add fail safe script to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Entire script should fail if any commands returns non-zero exit code
+# https://stackoverflow.com/questions/821396/aborting-a-shell-script-if-any-command-returns-a-non-zero-value
+set -e
 # Feel free to add migrations or whatever here
 yarn migrate:dev init
 # In production don't run migrate:dev BUT do run migrate:prod
